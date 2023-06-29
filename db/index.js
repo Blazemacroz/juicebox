a54e247a454cf9d7ad2cb1728d26836b4ab9fc9b
Sage <blazemacroz@gmail.com>
2023-06-28 19:36:04 -0500

    first databade made

---
+++ db/index.js
@@ -0,0 +1,40 @@
+const { Client } = require('pg');
+
+const client = new Client({
+    host: "localhost",
+    port: 5432,
+    database: "juicebox-dev",
+    user: "postgres",
+    password: "tolkien"
+});
+
+async function createUser({ username, password }) {
+    try {
+      const { rows } = await client.query(`
+        INSERT INTO users(username, password)
+        VALUES ($1, $2)
+        ON CONFLICT (username) DO NOTHING
+        RETURNING *;
+      `, [username, password]);
+  
+      return rows;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+async function getAllUsers () {
+    const { rows } = await client.query(
+        `SELECT id, username
+        
+        FROM users;
+        `
+    );
+
+    return rows;
+}
+module.exports = {
+    client,
+    getAllUsers,
+    createUser,
+  }
